d653d79895f367c75f6818301d8d6f1071025fc2
Juan Calvopina <juan.calvopina@gmail.com>
2023-02-14 15:49:33 -0500

    Implementing a component carousel with class component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import Carousel from './components/Carousel';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p1>Carousel</p1>
+      <div>
+        <Carousel>
+          <button name='button component' type='button'>OK</button>
+
+          <img name='image component' src={logo} alt='ReactJS' />
+
+          <form name='form component' action='call-service' method='post'>
+            <input type='text' placeholder='enter the username' /><br />
+            <input type='password' placeholder='enter the password' /><br />
+            <input type='submit' value='Login' />
+          </form>
+
+          <table name='table component'>
+            <thead>
+              <tr>
+                <th colspan='2'>The table header</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>The table body</td>
+                <td>with two columns</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <textarea name='text area component' rows='5' cols='33' placeholder='Type in this area' /> 
+
+        </Carousel>
+      </div>
     </div>
   );
 }
